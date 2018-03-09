1809e5cb8fb6a66725c380085abd381f31365052
Michael <31109433+Neal956@users.noreply.github.com>
2018-03-09 13:18:17 +0200

    Added new functions.

--- shadow-removal.py
+++ shadow-removal.py
@@ -5,8 +5,10 @@ import cv2
 # read an original image
 or_img = cv2.imread('shadow.jpg')
 
-# convert the RGB image to a LAB image
-lab_img = cv2.cvtColor(or_img, cv2.COLOR_RGB2LAB)
+rows, columns, channels = or_img.shape
+
+# convert the BGR image to a LAB image
+lab_img = cv2.cvtColor(or_img, cv2.COLOR_BGR2LAB)
 
 # channels of the converted image will be split into their individual planes
 l, a, b = cv2.split(lab_img)
@@ -23,6 +25,15 @@ a_mean_value = np.mean(a)
 # get mean value of the pixels in B plane
 b_mean_value = np.mean(b)
 
+# get standard deviation of channel in B plane
+b_std = np.std(b)
+
+# get a const for l to remove the shadow, (114.6 is mean value of the pixels in shadow)
+const_l = (l_mean_value + l_std) - 114.6
+
+# get a const for b to remove the shadow, (131.7 is mean value of the pixels in shadow)
+const_b = (b_mean_value + b_std ) - 131.7
+
 
 # the pixels with a value in L ≤ (mean (L) – standard deviation (L)/3)
 def l_shadow_pixels_classifier():
@@ -31,7 +42,13 @@ def l_shadow_pixels_classifier():
 
 # the pixels with lower values in both L and B planes
 def lb_shadow_pixels_classifier():
-    print("dd")
+    for i in range(rows):
+        for j in range(columns):
+            l_px = lab_img[i, j, 0]
+            a_px = lab_img[i, j, 1]
+            b_px = lab_img[i, j, 2]
+            if l_px < l_mean_value - (l_std / 3) and b_px < b_mean_value - (b_std / 3):
+                lab_img[i, j] = [l_px + const_l, a_px, b_px + const_b]
 
 
 # if mean(A) + mean(B) ≤ 256, then classify...
@@ -41,9 +58,12 @@ if a_mean_value + b_mean_value <= 256:
 else:
     lb_shadow_pixels_classifier()
 
+mask1 = cv2.cvtColor(lab_img, cv2.COLOR_LAB2BGR)
 
 # show both images
-cv2.imshow("image", lab_img)
+cv2.imshow("image1", or_img)
+cv2.imshow("image2", mask1)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
